9efdbc3cace768398caeb43b4682f0392ac7b9be
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-26 18:06:27 -0300

    Arquivos gerados na criação do projeto

--- ProjetoVS_Um/CSharp/3-VariaveisPontoFlutuante/Program.cs
+++ ProjetoVS_Um/CSharp/3-VariaveisPontoFlutuante/Program.cs
@@ -13,7 +13,7 @@ class Program
         Console.WriteLine(salario);
 
         /*Uma variável do tipo double aceita um número inteiro, mas uma variável inteiro não aceita um número com ponto flutuante.
-          Quando fazemos uma divisão de dois inteiros, mas os declaram como double pois o resultado será um número com ponto flutuante, obteremos como resposta um número também inteiro.
+          Quando fazemos uma divisão de dois inteiros, mas os declaram como double pois o resultado será um número com ponto flutuante, obteremos como resposta um número também inteiro (não é um arredondamento).
           Para evitar isso, basta colocar o número inteiro seguido por .0 (por exemplo 9.0)
         */
 
