09a15d8e64cbd0273d3fbf1478a12cb7e8e015b2
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-26 15:12:14 -0300

    Novo projeto para demonstrar como criar uma variável

---
+++ ProjetoVS_Um/CSharp/CriandoVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+
+        int idade;
+        idade = 27;
+        Console.WriteLine("Ana tem " + idade + " anos"); /*ConsoleWriteLine => faz um print do resultado e pula uma linha*/
+
+        idade = 27 - 5;
+        Console.WriteLine(idade);
+
+        idade = 5 * 2 - 6;
+        Console.WriteLine(idade);
+
+        idade = (5 - 2) * 3;
+        Console.Write(idade);/*ConsoleWrite => faz um print do resultado, mas não pula uma linha*/
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}/*https://www.alura.com.br/artigos/convencoes-nomenclatura-camel-pascal-kebab-snake-case*/
