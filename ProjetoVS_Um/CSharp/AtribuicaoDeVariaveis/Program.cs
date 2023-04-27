acbd163e2dce3fb03ffc9213af4517bfbc52a98a
Amanda Marques Pardinho <amanda.pardinho@performait.com>
2023-04-27 15:09:02 -0300

    Projeto exemplificando como é o fluxo de atribuição de uma variável

---
+++ ProjetoVS_Um/CSharp/AtribuicaoDeVariaveis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 6 - Atribuição de Variáveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
