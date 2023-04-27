0daa5655378a2b380ec9b7be06a5dedb02519d0b
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-27 16:42:58 -0300

    Apresentação da variável booleana

---
+++ ProjetoVS_Um/CSharp/Condicionais2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 8 - Condicionais 2");
+
+        int idadeJuliana = 16;
+        int quantidadeDePessoas = 2;
+        bool grupo = quantidadeDePessoas > 1;
+
+        if (idadeJuliana >= 18 || grupo)
+        {
+            Console.WriteLine("Pode entrar!");
+        }else
+        {
+            Console.WriteLine("Lamento, mas não pode entrar");
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
