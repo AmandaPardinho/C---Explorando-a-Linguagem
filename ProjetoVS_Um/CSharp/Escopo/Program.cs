4c0ae032f02938db11af0eca3e1601aa38754063
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-27 17:21:37 -0300

    Novo projeto para explocar escopo global vs escopo local

---
+++ ProjetoVS_Um/CSharp/Escopo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 9 - Escopo");
+
+        int idadeVitor = 30;
+        int quantidadeDePessoas = 2;
+        bool estaAcompanhado = quantidadeDePessoas > 1;
+
+        string textoAdicional; // variável de escopo global
+
+        if(estaAcompanhado == true)
+        {
+            /*string => gera uma variável de escopo local*/textoAdicional = "Vitor está acompanhado";
+        }
+        else
+        {
+            /*string => gera uma variável de escopo local*/
+            textoAdicional = "Vitor não está acompanhado";
+        }
+
+        if(idadeVitor >= 18 || estaAcompanhado)
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Pode entrar");
+        }
+        else
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Não pode entra");
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
