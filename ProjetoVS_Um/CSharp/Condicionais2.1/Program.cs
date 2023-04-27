2f4aa234b2dea2181dd280437c5caf01abfb7cb0
Amanda Marques Pardinho <amanda.pardinho@performait.com>
2023-04-27 16:07:34 -0300

    Resolução do problema dado em aula, porém com um código ligeiramente diferente

---
+++ ProjetoVS_Um/CSharp/Condicionais2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 1.2 - Condicionais");
+
+        int idade = 16;
+        int quantidadeDePessoas = 2;
+        bool estaAcompanhado = false;
+
+        if(idade >= 18)
+        {
+            Console.WriteLine("Pode entrar!");
+        }else if/*(quantidadeDePessoas > 1)*/(estaAcompanhado)
+        {
+            Console.WriteLine("Está acompanhado. Pode entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
