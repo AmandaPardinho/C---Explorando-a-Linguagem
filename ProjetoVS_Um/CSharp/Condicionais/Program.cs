3fa48a2a70828e088d1e436c6d0215503be9e715
Amanda Marques Pardinho <amanda.pardinho@performait.com>
2023-04-27 16:08:11 -0300

    Resolução do exercício sobre condicionais

---
+++ ProjetoVS_Um/CSharp/Condicionais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadeDePessoas = 2;
+        bool estaAcompanhado = true;
+
+        if(idadeJoao >= 18)
+        {
+            Console.WriteLine("Pode entrar");
+        }          
+        else
+        {
+            /*if(quantidadeDePessoas > 1)
+            {
+                Console.WriteLine("Está acompanhado. Pode entrar!");
+            }*/ //solução acima foi a feita na aula
+            if(estaAcompanhado == true)
+            {
+                Console.WriteLine("Está acompanhado. Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }    
+        }
+
+        Console.WriteLine("Tecle enter para sair");
+        Console.ReadLine();
+    }
+}
