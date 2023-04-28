b6f061c03d584690f0c23259aca8e86920fecb71
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 18:41:25 -0300

    Exercício extra - for aninhado

---
+++ ProjetoVS_Um/CSharp/ExercicioExtraForAninhado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Common;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int coluna = 0;
+
+        for(int linha = 0; linha < 5; linha++)
+        {
+            for (coluna = 0; coluna < 5; coluna++)
+            {
+                if (coluna > linha)
+
+                    break;
+
+                Console.Write(coluna + 1);
+            }
+            Console.WriteLine();
+        }
+        
+
+        Console.WriteLine("Tecle enter para sair");
+        Console.ReadLine();
+    }
+}
