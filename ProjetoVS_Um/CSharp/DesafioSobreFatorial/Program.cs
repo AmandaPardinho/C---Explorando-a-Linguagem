a6a1276e8721e4cb0613bb841566b52d0e86c1b2
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 20:55:47 -0300

    Desafio de cálculo de fatorial

---
+++ ProjetoVS_Um/CSharp/DesafioSobreFatorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Desafio 3 - Fatorial");
+
+        //Agora crie uma nova classe, escreva um for que inicie uma variável n (número atual) como 1 e fatorial (resultado total) como 1. Faça seu laço entre 1 a 10 e calcule o resultado!
+
+        double n = 10;
+        double fatorial = 1;
+
+        for(int i = 1; i <= n; i++)
+        {
+            fatorial *= i;
+            Console.WriteLine(fatorial);
+        }
+        
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
+
