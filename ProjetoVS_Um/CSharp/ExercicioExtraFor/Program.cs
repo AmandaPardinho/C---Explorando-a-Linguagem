fc522e16592e63b7725a69ede4a4dd4a70721248
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 17:32:33 -0300

    Exercício extra - for

---
+++ ProjetoVS_Um/CSharp/ExercicioExtraFor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exercício Extra - For");
+
+        //Escreva um contador que conte de 0 a 10, usando o "for"
+
+        for(int contador = 0; contador <= 10; contador++)
+        {
+            Console.WriteLine(contador);
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
