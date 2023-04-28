42bcd1b46c5fce88fa32361d33f75cc478251649
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 12:31:10 -0300

    Laço de repetição while

---
+++ ProjetoVS_Um/CSharp/CalculaPoupanca/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 10 - Calcula Poupança");
+
+        double investimento = 1000;
+
+        //Rendimento de 0.5% (= 0.005) ao mês
+        /*investimento = investimento + investimento * 0.005;
+        Console.WriteLine(investimento);*/
+
+        //Calculando o rendimento em 12 meses
+        int mes = 1;
+        while(mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " você terá R$" + investimento);
+
+            mes++;
+        }
+
+
+
+
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
