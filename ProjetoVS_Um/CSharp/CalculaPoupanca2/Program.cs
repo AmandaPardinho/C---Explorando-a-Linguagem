d8359bac14b8bcbccae4763df39f4bcc46f29006
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 13:04:24 -0300

    Laço de repetição for

---
+++ ProjetoVS_Um/CSharp/CalculaPoupanca2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 11 - Calcula Poupança 2");
+
+        double investimento = 1000;
+        
+        for(int mes = 1; mes <= 12; mes++)
+        {
+            investimento = investimento + (investimento * 0.005);
+            Console.WriteLine("No mês " + mes + " você terá R$ " + investimento);
+        }
+
+
+
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
