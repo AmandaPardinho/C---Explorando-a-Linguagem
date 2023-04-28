b827276339537f445c3b2c87ac5b165ad1175f8e
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 18:42:10 -0300

    Desafio 3 - múltiplos de 3

---
+++ ProjetoVS_Um/CSharp/Desafio2Multiplos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Desafio 2 - Múltiplos de 3");
+
+        //Imprimir todos os múltiplos de 3 entre 0 e 100
+
+        for(int numero = 1; numero <= 100; numero++)
+        {
+            if ((numero % 3) == 0)
+                Console.WriteLine(numero);
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
