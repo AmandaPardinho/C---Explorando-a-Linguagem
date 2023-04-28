471dd4f0428ae59d2ac47e057b81214c9d7ae496
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 14:28:01 -0300

    Entendendo encadeamento de laços for

---
+++ ProjetoVS_Um/CSharp/InvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 12 - Investimento a Longo Prazo");
+
+        double investimento = 1000;
+        double fatorRendimento = 1.005;
+
+        for(int anos = 1; anos <= 5; anos++)
+        {
+            for(int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= fatorRendimento;
+            }
+            fatorRendimento += 0.001;
+        }
+
+        Console.WriteLine("Depois de 5 anos você terá R$ " + investimento);
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
