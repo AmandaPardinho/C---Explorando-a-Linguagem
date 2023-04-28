279ebeea9e69c9d18875c375d33c97a50b61800c
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 17:00:39 -0300

    Mais um exercício com for encadeado

---
+++ ProjetoVS_Um/CSharp/EncadeandoFor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13 - Encadeando For");
+        /* Criando o seguinte desenho:
+         *   *
+         *   * *
+         *   * * * 
+         *   * * * *
+         *   * * * * *      
+         */
+
+        for (int contadorDeLinhas = 0; contadorDeLinhas < 10; contadorDeLinhas++)
+        {
+            for(int contadorDeColunas = 0; contadorDeColunas < 10; contadorDeColunas++)
+            {
+                Console.Write("* ");
+                if (contadorDeColunas >= contadorDeLinhas)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        //Outra solução =>
+        Console.WriteLine();
+
+        for(int contadorDeLinhas = 0; contadorDeLinhas < 10; contadorDeLinhas++)
+        {
+            for(int contadorDeColunas = 0; contadorDeColunas <= contadorDeLinhas; contadorDeColunas++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
