a78b9332f3a3fd8e49e4f35a73af24ad48b84529
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 17:27:09 -0300

    Exercício extra - while

---
+++ ProjetoVS_Um/CSharp/ExercicioExtraWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exercício Extra - While");
+
+        //Escreva um contador que imprima os números de 0 a 10
+
+        int contador = 0;
+
+        while(contador <= 10)
+        {
+            Console.WriteLine(contador);
+            contador++;
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
