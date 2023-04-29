697bb08b1e2b6a813fc88eb64f40a943b7a4a8b3
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-28 21:09:40 -0300

    Exemplo de uso de do-while

---
+++ ProjetoVS_Um/CSharp/DoWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto - Do - While");
+
+        //Escreva um contador que imprima os números de 0 a 10
+
+        int number = 0;
+
+        do
+        {
+            Console.WriteLine(number);
+            number++;
+        } while (number <= 10);
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
